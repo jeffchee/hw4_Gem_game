7f70f388bbce95cb38831104b8808f0cc93f1695
JeffChee <jeffrey.chee@yahoo.com>
2018-08-20 09:55:49 -0700

    dfa

--- Gem_J.js
+++ Gem_J.js
@@ -6,6 +6,28 @@
 
 
 
-function myFunction1() {
-  document.getElementById("myFunction1").innerHTML = "Hello World";
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
+var randomNum = getRndInteger(19, 120);
+getRndInteger(19, 120);
+console.log(randomNum);
+
+
+
+
+
+// document.getElementById("randomNum").innerHTML = randomNum;
+
+// function myFunction1() {
+//   document.getElementById("myFunction1").innerHTML = ...var...;
+// }
+// function myFunction2() {
+//   document.getElementById("myFunction2").innerHTML = ...var...;
+// }
+// function myFunction3() {
+//   document.getElementById("myFunction3").innerHTML = ...var...;
+// }
+// function myFunction4() {
+//   document.getElementById("myFunction4").innerHTML = ...var...;
+// }
